e86dca9bc4e4a35bcd8acb70557a6cf1911ecaff
rahmanabdulquadir <rahmanaq777@gmail.comm>
2023-02-18 21:44:38 +0600

    navbar added

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,50 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="navbar bg-base-200">
+      <div className="flex-1">
+        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+      </div>
+      <ul className="menu menu-horizontal px-1 font-semibold text-l ">
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="flex-none gap-2">
+        <div className="dropdown dropdown-end">
+          <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <div className="w-10 rounded-full">
+              <img src="https://avatars.githubusercontent.com/u/108421979?s=400&u=8775340035877c8e6e7fb020478e23ed9e308c85&v=4" />
+            </div>
+          </label>
+          <ul
+            tabIndex={0}
+            className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
