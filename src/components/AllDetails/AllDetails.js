0eceb05da13c367149a9a3aed88495128fbb7d94
rahmanabdulquadir <rahmanaq777@gmail.comm>
2023-02-20 18:27:52 +0600

    project completed

--- src/components/AllDetails/AllDetails.js
+++ src/components/AllDetails/AllDetails.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PersonalInfo from '../PersonalInfo/PersonalInfo';
+import './AllDetails.css'
 
 const AllDetails = ({singleActivity}) => {
+
+  const getTimeFromLs = () => {
+    const storedTime = localStorage.getItem('break-time')
+    return storedTime
+  }
+  const [addBreak, setAddBreak] = useState(getTimeFromLs())
+
+  
+  const handleAddToBreak = (time) => {
+    localStorage.setItem('break-time', time);
+    setAddBreak(time);
+  }
   let totalTime = 0
   for(const act of singleActivity){
     totalTime = totalTime + parseInt(act.time)
     console.log(totalTime)
   }
   return (
-    <div className='col-span-2'>
+    <div className='col-span-2 side-cart'>
       <PersonalInfo></PersonalInfo>
-      <div className='mt-7'>
+      <div className='mt-7 stick'>
         <h1 className='text-xl font-semibold'>Add a break</h1>
         <div className='flex justify-evenly bg-slate-200 py-8 px-14 rounded-md mt-3'>
-          <h2 className='text-l mr-3 p-3 bg-white rounded-full font-bold'>10m</h2>
-          <h2 className='text-l mr-3 p-3 bg-white rounded-full font-bold'>20m</h2>
-          <h2 className='text-l mr-3 p-3 bg-white rounded-full font-bold'>30m</h2>
-          <h2 className='text-l mr-3 p-3 bg-white rounded-full font-bold'>40m</h2>
-          <h2 className='text-l  p-3 bg-white rounded-full font-bold'>50m</h2>
+          <h2 onClick={() => handleAddToBreak(10)} className='text-l mr-3 p-3 cursor-pointer	 bg-white rounded-full font-bold'>10m</h2>
+          <h2 onClick={() => handleAddToBreak(20)} className='text-l mr-3 p-3 cursor-pointer	 bg-white rounded-full font-bold'>20m</h2>
+          <h2 onClick={() => handleAddToBreak(30)} className='text-l mr-3 p-3 cursor-pointer	 bg-white rounded-full font-bold'>30m</h2>
+          <h2 onClick={() => handleAddToBreak(40)} className='text-l mr-3 p-3 cursor-pointer	 bg-white rounded-full font-bold'>40m</h2>
+          <h2 onClick={() => handleAddToBreak(50)} className='text-l  p-3 cursor-pointer	 bg-white rounded-full font-bold'>50m</h2>
         </div>
       </div>
 
@@ -30,7 +43,7 @@ const AllDetails = ({singleActivity}) => {
           </div>
           <div className='flex justify-between bg-slate-200 py-5 px-4 mt-3 rounded-md mt-3'>
             <h1 className='text-l font-semibold'>Break Time</h1>
-            <p>{totalTime} Min</p>
+            <p>{addBreak} Min</p>
           </div>
         </div>
       </div>
